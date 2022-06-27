9364ea04e0d1724181c04f1de33c813513af8ac8
Luísa Ferro <81780290+LuisaFerro@users.noreply.github.com>
2022-06-27 16:12:15 -0300

    27.6 - variáveis organizadas

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import sys
 
 
 
-def vira_verde(valor_esq,valor_dir,verde):
+def vira_verde(valor_esq,valor_dir):
     #motores.on_for_seconds(SpeedPercent(-20), SpeedPercent(-20), 1.2)
     global verde_esq
+    global verde
     valor_esq = sensorEsq.value()
     valor_dir = sensorDir.value()
 
@@ -21,7 +22,7 @@ def vira_verde(valor_esq,valor_dir,verde):
         motores.on_for_seconds(SpeedPercent(-50),SpeedPercent(-50),0.3)
         motores.on_for_seconds(SpeedPercent(-10),SpeedPercent(-50),0.8) #vira esquerda
         valor_esq = sensorEsq.value()
-        segue_linha(preto)
+        segue_linha()
     
     elif (valor_dir < verde): #dir ve verde
         Sound().beep()
@@ -29,7 +30,7 @@ def vira_verde(valor_esq,valor_dir,verde):
         motores.on_for_seconds(SpeedPercent(-50),SpeedPercent(-50),0.4)
         motores.on_for_seconds(SpeedPercent(-50),SpeedPercent(-10),0.8) #vira direita
         valor_dir = sensorDir.value()
-        segue_linha(preto)
+        segue_linha()
         #motores.on_for_seconds(SpeedPercent(-50),SpeedPercent(30),1) #dobra pra direita
 
 def ambient_light_intensity(self):
@@ -39,26 +40,18 @@ def ambient_light_intensity(self):
         self._ensure_mode(self.MODE_AMBIENT)
         return self.value(0) * self._scale('AMBIENT')
 
+def segue_linha():
+    global preto
+    global branco
+    global branco_meio
+    global objetivo
+    global vmenor
+    global vmaior
+    global verde_esq
 
-def segue_linha(preto):
-    #verde = 10 #12
-    valor_frontal = sensorFrontal.value()
-
-    if valor_frontal <= 200: #<= 100 no modo ambiente // no claro, o valor é 250
-        #obstaculos(preto)
-        pass
-        '''valor_frontal = sensorFrontal.value()'''
-        
     valor_esq = sensorEsq.value()
     valor_dir = sensorDir.value() 
     valor_meio = sensorMeio.value()
-
-    branco = 160 #dentro da sala
-    branco_meio = 500 #600 às 18hrs // 550 às 14hrs
-    objetivo = 450 #450 às 14hrs
-    vmenor = 10 #42 #esses valores deixam a curv mais aberta, se a distância entre os valores for alta. O que é bom na curva do quadrado
-    vmaior = -35#valores: 35,-75 // 42 e -30
-    #bom valor de agressividade = 60 e -60 // 70 e -70
 
     sensorMeio_valores.append(valor_meio)
     sensorDir_valores.append(valor_dir)
@@ -67,11 +60,18 @@ def segue_linha(preto):
     print(sensorEsq_valores, file=sys.stderr)
     #print(sensorDir_valores, file=sys.stderr)
     #print(sensorMeio_valores, file=sys.stderr)
+
+    #verde = 10 #12
+    valor_frontal = sensorFrontal.value()
+
+    if valor_frontal <= 200: #<= 100 no modo ambiente // no claro, o valor é 250
+        #obstaculos(preto)
+        pass
+        '''valor_frontal = sensorFrontal.value()'''
     
-    global verde_esq 
 
     if (valor_dir < verde) or (valor_esq < (verde_esq)): #um ou outro ve verde
-        vira_verde(valor_esq,valor_dir,verde)
+        vira_verde(valor_esq,valor_dir)
     
     elif (valor_meio < branco_meio): #sensor do meio vendo preto
         kp = 0.17 #0.2
@@ -117,7 +117,7 @@ def segue_linha(preto):
         else: #todos veem branco
             motores.on(SpeedPercent(-20),SpeedPercent(-20))
 
-def obstaculos(preto):
+def obstaculos():
     valor_esq = sensorEsq.value()
     valor_dir = sensorDir.value()
     valor_meio = sensorMeio.value()
@@ -140,7 +140,67 @@ def calibra_sensores():
     calibrate_white(sensorEsq)
     calibrate_white(sensorDir)
     
-    
+def calibra_verde():
+    global branco
+
+    valor_esq = sensorEsq.value()
+    valor_dir = sensorDir.value()
+
+    passou_branco = False
+
+    while passou_branco == False:
+        motores.on(SpeedPercent(-20), SpeedPercent(-20))
+
+        
+        valor_esq = sensorEsq.value()
+        valor_dir = sensorDir.value()
+
+        if valor_esq > branco or valor_dir > branco:
+            passou_branco = True
+
+        if passou_branco == True:
+            while valor_esq > branco or valor_dir > branco:
+                motores.on(SpeedPercent(-20), SpeedPercent(-20))
+            while valor_esq < branco or valor_dir < branco:
+                valor_esq = sensorEsq.value()
+                valor_dir = sensorDir.value()
+                pretoEsq_valores.append(valor_esq)
+                pretoDir_valores.append(valor_dir)
+        else:
+            while valor_esq < branco or valor_dir < branco:
+                valor_esq = sensorEsq.value()
+                valor_dir = sensorDir.value()
+                verdeEsq_valores.append(valor_esq)
+                verdeDir_valores.append(valor_dir)
+
+
+        '''while len(verdeEsq_valores) < 10:
+        motores.on(SpeedPercent(-20), SpeedPercent(-20))
+        
+        valor_esq = sensorEsq.value()
+        valor_dir = sensorDir.value()
+
+        if valor_esq > branco or valor_dir > branco:
+            passou_branco = True
+            
+        if passou_branco == True:
+            while valor_esq < branco or valor_dir < branco:
+                valor_esq = sensorEsq.value()
+                valor_dir = sensorDir.value()
+                pretoEsq_valores.append(valor_esq)
+                pretoDir_valores.append(valor_dir)
+        else:
+            while valor_esq < branco or valor_dir < branco:
+                valor_esq = sensorEsq.value()
+                valor_dir = sensorDir.value()
+                verdeEsq_valores.append(valor_esq)
+                verdeDir_valores.append(valor_dir)'''
+    print("SensorEsquerdo - verde = {} ".format(verdeEsq_valores),file=sys.stderr)
+    print("SensorDireito - verde = {} ".format(verdeDir_valores), file=sys.stderr)
+    print("SensorEsquerdo - preto = {} ".format(pretoEsq_valores),file=sys.stderr)
+    print("SensorDireito - preto = {} ".format(pretoDir_valores),file=sys.stderr)
+
+
 def abaixa_garra():
     garra.on_for_seconds(SpeedPercent(-15), 0.4)
     garra.on_for_seconds(SpeedPercent(-9), 0.7)
@@ -165,7 +225,7 @@ def funcao_garra():
 
 def leitor_cor():
     while True:
-        print("SensorEsquerdo = {} ".format(sensorEsq.value())) #valores esq: branco (59) // preto (10) // verde (4 - 9)
+        print("SensorEsquerdo = {} ".format(sensorEsq.value(),file=sys.stderr)) #valores esq: branco (59) // preto (10) // verde (4 - 9)
         print("SensorDireito = {} ".format(sensorDir.value()), file=sys.stderr)# valores dir: branco (70) // preto (7) // verde (4) // verde é menor que 6
 
 def valores_sCor(sensorEsq,sensorDir):
@@ -180,9 +240,6 @@ def valores_sCor(sensorEsq,sensorDir):
     print(sensorDir_valores, file=sys.stderr)
     print(sensorEsq_valores, file=sys.stderr)
 
-def descobre_verde():
-
-
 
 def inicio():
     #calibra_sensores()
@@ -190,10 +247,14 @@ def inicio():
         valor_frontal = sensorFrontal.value()     
         sensorFrontal_valores.append(valor_frontal)
 
-        segue_linha(preto)
+        segue_linha()
         #motores.follow_line(kp = 0.6 , ki =1 , kd =1 , speed=SpeedPercent(30), 500, True, 600)
-        
-        
+
+
+
+      
+ #Definição de motores e sensores
+
 motores = MoveTank(OUTPUT_A,OUTPUT_B)
 garra = Motor(OUTPUT_C)
 estoque = Motor(OUTPUT_D)
@@ -203,24 +264,32 @@ sensorDir = ColorSensor(INPUT_1)
 sensorMeio = LightSensor(INPUT_4)
 sensorFrontal = LightSensor(INPUT_2)
 #sensorFrontal = Sensor(INPUT_2)
+
+#Definição de Listas
+
 sensorEsq_valores = []
 sensorDir_valores = []
 sensorMeio_valores = []
 sensorFrontal_valores = []
+verdeEsq_valores = []
+verdeDir_valores = []
+pretoEsq_valores = []
+pretoDir_valores = []
+
+#Definição de variáveis
+
+preto = 45
+branco = 130 #160
+branco_meio = 600 #600 às 16 e 18hrs // 550 às 14hrs // 500 às ??
+objetivo = 500 #450 às 14hrs // 500 às 16
+vmenor = 10 #42 #esses valores deixam a curv mais aberta, se a distância entre os valores for alta. O que é bom na curva do quadrado
+vmaior = -35#valores: 35,-75 // 42 e -30 -> bom valor de agressividade = 60 e -60 // 70 e -70
 verde = 16 #16 às 14hrs
 verde_esq = verde + 12 #+ 6 + 2
 
-
-def feira_livro():
-    while True:
-        print(sensorFrontal.value(), file=sys.stderr)
-        if sensorFrontal.value() < 20:
-            funcao_garra()
-
-preto = 45
-
 inicio()
-
+#calibra_sensores()
+#calibra_verde()
 
 #le 45 por segundo no branco
 
